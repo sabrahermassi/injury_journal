286cf600570adbabb7979edbd1d91ad0dd7314f1
sabra <sabra.herm@gmail.com>
2026-08-07 21:20:59 +0200

    Update injury schema

---
+++ backend/prisma/migrations/20260807191940_update_injury_schema/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Injury" ALTER COLUMN "description" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "MedicalVisit" ALTER COLUMN "doctor" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Symptom" ALTER COLUMN "location" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
